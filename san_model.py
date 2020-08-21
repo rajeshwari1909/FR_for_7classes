e983adea264075e2e1b15b8eff11762308f20487
rajeshwari1909 <48403248+rajeshwari1909@users.noreply.github.com>
2020-08-21 12:32:10 +0530

    Add files via upload

---
+++ san_model.py
@@ -0,0 +1,82 @@
+from __future__ import print_function
+import keras
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Activation,Flatten,BatchNormalization
+from keras.layers import Conv2D,MaxPooling2D
+import os
+
+
+num_classes = 5
+img_rows,img_cols = 48,48
+batch_size = 100
+
+
+
+model = Sequential()
+
+# Block-1
+
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(32,(3,3),padding='same',kernel_initializer='he_normal',input_shape=(img_rows,img_cols,1)))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-2 
+
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(64,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-3
+
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(128,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-4 
+
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256,(3,3),padding='same',kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2,2)))
+model.add(Dropout(0.2))
+
+# Block-5
+
+model.add(Flatten())
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-6
+
+model.add(Dense(64,kernel_initializer='he_normal'))
+model.add(Activation('elu'))
+model.add(BatchNormalization())
+model.add(Dropout(0.5))
+
+# Block-7
+
+model.add(Dense(num_classes,kernel_initializer='he_normal'))
+model.add(Activation('softmax'))
+
+print(model.summary())
